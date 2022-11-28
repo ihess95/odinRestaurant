e986c4bbd00a9ac0c0d3024c79f05317a26d4cb6
Ian Hess <ian.h.hess@gmail.com>
2022-11-28 11:50:43 -0500

    working on linking and creating js

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("this is a test");
+console.log("this is a test"),function(){const t=document.querySelector(".content"),e=document.createAttribute("h1");e.textContent="Ian's Spaghetti Parlor",e.classList.add("restaurantHeader"),t.appendChild(e)}();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+// import { createBase } from "./pageLoad";
+
 console.log("this is a test");
+
+function createBase() {
+  const bodyContainer = document.querySelector(".content");
+  const header = document.createAttribute("h1");
+  header.textContent = "Ian's Spaghetti Parlor";
+  header.classList.add("restaurantHeader");
+
+  bodyContainer.appendChild(header);
+}
+
+createBase();
