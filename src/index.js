e889e44b4a844b4813dfd3d4ab8b4b3621c15cb6
Ian Hess <ian.h.hess@gmail.com>
2022-11-29 11:56:18 -0500

    create modules for other pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import { createBase } from "./pageLoad";
+// import { createBase } from "./pageLoad";
 
-console.log("this is a test");
+// console.log("this is a test");
 
-createBase();
+// createBase();
+function createButtons() {
+  const webContainer = document.querySelector(".webContainer");
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("buttonContainer");
+  const homeBtn = document.createElement("button");
+  homeBtn.textContent = "Home";
+  const menuBtn = document.createElement("button");
+  menuBtn.textContent = "Menu";
+  const contactBtn = document.createElement("button");
+  contactBtn.textContent = "Contact Us";
+  buttonContainer.appendChild(homeBtn);
+  buttonContainer.appendChild(menuBtn);
+  buttonContainer.appendChild(contactBtn);
+  webContainer.appendChild(buttonContainer);
+  webContainer.appendChild(document.getElementById("content"));
+}
+createButtons();
---
+++ src/menu.js
@@ -0,0 +1,18 @@
+function createMenu() {
+  const bodyContainer = document.getElementById("content");
+  const header = document.createElement("div");
+  header.textContent = "Ian's Menu Parlor";
+  header.classList.add("restaurantHeader");
+  const pic = document.createElement("img");
+  pic.src = "../img/dump.jpg";
+  pic.classList.add("dumpImg");
+  const bodyText = document.createElement("div");
+  bodyText.textContent =
+    "Woah this restaurant is so good I almost forgot it's called a spaghetti parlor when all they serve is dumplings!";
+
+  bodyContainer.appendChild(header);
+  bodyContainer.appendChild(pic);
+  bodyContainer.appendChild(bodyText);
+}
+
+export { createMenu };
