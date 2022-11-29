49965bf4923be133c48411a3613a35599310ba29
Ian Hess <ian.h.hess@gmail.com>
2022-11-29 11:08:05 -0500

    fix error, defer js in index

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,1 @@
-function createBase() {
-  const bodyContainer = document.getElementById("content");
-  const header = document.createElement("h1");
-  header.textContent = "Ian's Spaghetti Parlor";
-  header.classList.add("restaurantHeader");
-
-  bodyContainer.appendChild(header);
-}
-
-createBase();
+console.log("this is a test"),createBase();
--- src/index.js
+++ src/index.js
@@ -2,13 +2,4 @@
 
 console.log("this is a test");
 
-function createBase() {
-  const bodyContainer = document.querySelector(".content");
-  const header = document.createElement("h1");
-  header.textContent = "Ian's Spaghetti Parlor";
-  header.classList.add("restaurantHeader");
-
-  bodyContainer.appendChild(header);
-}
-
 createBase();
