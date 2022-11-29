1f6c4400a13decd7f86e3920f86a99b94c3f8db1
Ian Hess <ian.h.hess@gmail.com>
2022-11-29 11:20:48 -0500

    add image and skeleton

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -3,7 +3,15 @@ function createBase() {
   const header = document.createElement("div");
   header.textContent = "Ian's Spaghetti Parlor";
   header.classList.add("restaurantHeader");
+  const pic = document.createElement("img");
+  pic.src = "../img/dump.jpg";
+  pic.classList.add("dumpImg");
+  const bodyText = document.createElement("div");
+  bodyText.textContent =
+    "Woah this restaurant is so good I almost forgot it's called a spaghetti parlor when all they serve is dumplings!";
 
   bodyContainer.appendChild(header);
+  bodyContainer.appendChild(pic);
+  bodyContainer.appendChild(bodyText);
 }
 export { createBase };
