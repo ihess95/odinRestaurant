f06ffcf399cf702645b0fb49215de659f0845b4e
Ian Hess <ian.h.hess@gmail.com>
2022-11-30 11:43:22 -0500

    force myself to use a factory for dishes

--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,62 @@
 function createMenu() {
   const bodyContainer = document.getElementById("content");
   const header = document.createElement("div");
+  const menuGrid = document.createElement("div");
+  menuGrid.classList.add("menuGrid");
   header.textContent = "Ian's Menu Parlor";
   header.classList.add("restaurantHeader");
-  const pic = document.createElement("img");
-  pic.src = "../img/dump.jpg";
-  pic.classList.add("dumpImg");
-  const bodyText = document.createElement("div");
-  bodyText.textContent =
-    "Woah this restaurant is so good I almost forgot it's called a spaghetti parlor when all they serve is dumplings!";
+
+  function createFood(foodType, foodName, foodImg, foodDesc) {
+    // const foodCard = foodType + "Card";
+    const foodCard = document.createElement("div");
+    const foodTitle = document.createElement("div");
+    const foodPic = document.createElement("img");
+    const foodText = document.createElement("div");
+
+    foodCard.classList.add(foodType + "Card");
+
+    foodTitle.classList.add(foodType + "Title");
+    foodTitle.textContent = foodName;
+
+    foodPic.src = foodImg;
+    foodPic.classList.add(foodType + "Img");
+
+    foodText.classList.add(foodType + "Desc");
+    foodText.textContent = foodDesc;
+
+    foodCard.appendChild(foodTitle);
+    foodCard.appendChild(foodPic);
+    foodCard.appendChild(foodText);
+    menuGrid.appendChild(foodCard);
+  }
+  //   const dumpCard = document.createElement("div");
+  //   dumpCard.classList.add("dumpCard");
+  //   const dump = document.createElement("img");
+  //   dump.src = "../img/dump.jpg";
+  //   dump.classList.add("dumpImg");
+  //   const dumpText = document.createElement("div");
+  //   dumpText.textContent = "Boy Howdy would ya look at these dumps";
 
   while (bodyContainer.hasChildNodes()) {
     bodyContainer.removeChild(bodyContainer.firstChild);
   }
   bodyContainer.appendChild(header);
-  bodyContainer.appendChild(pic);
-  bodyContainer.appendChild(bodyText);
+  bodyContainer.appendChild(menuGrid);
+  //   menuGrid.appendChild(dumpCard);
+  //   dumpCard.appendChild(dump);
+  //   dumpCard.appendChild(dumpText);
+  createFood(
+    "dump",
+    "Dumplings",
+    "../img/dump.jpg",
+    "Boy Howdy Look at these dumps"
+  );
+  createFood(
+    "lasagna",
+    "Lasagna",
+    "../img/lasagna.png",
+    "Hoo boy is that a whole lasagn?"
+  );
 }
 
 export { createMenu };
