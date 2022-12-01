4b66149b1e3ac049cc5c06566e06e0a35b7acbdb
Ian Hess <ian.h.hess@gmail.com>
2022-12-01 14:12:07 -0500

    add padding

--- src/contact.js
+++ src/contact.js
@@ -7,6 +7,7 @@ function createContact() {
   pic.src = "../img/contact.webp";
   pic.classList.add("dumpImg");
   const bodyText = document.createElement("div");
+  bodyText.classList.add("description");
   bodyText.textContent =
     "Woah this restaurant is so good I almost forgot it's called a spaghetti parlor when all they serve is dumplings!";
 
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -7,6 +7,7 @@ function createBase() {
   pic.src = "../img/chef.jpg";
   pic.classList.add("dumpImg");
   const bodyText = document.createElement("div");
+  bodyText.classList.add("description");
   bodyText.textContent =
     "Woah this restaurant is so good I almost forgot it's called a spaghetti parlor when all they serve is dumplings!";
 
